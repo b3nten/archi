ad45e78294bcf62b3ce5ebfeec925f6e20f21775
Benton Boychuk-Chorney <benten@Bentons-Air.gv.shawcable.net>
2023-12-15 21:17:41 -0800

    various updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import unocss from "unocss/vite";
 
 export default defineConfig({
   root: "./playground",
-  plugins: [unocss()],
+  server: {
+    port: 8000,
+  }
 });
