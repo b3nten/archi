0e4de8e97ce29faad2b630923af7ca14e8c1ce82
benten <ben@benten.ca>
2024-01-12 02:06:13 -0800

    various fixes

--- playground/browser.ts
+++
@@ -1,4 +0,0 @@
-const title = document.createElement("h1");
-title.innerText = "Hello, world!";
-
-document.body.append(title);
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  root: "./playground",
-  server: {
-    port: 8000,
-  }
-});
